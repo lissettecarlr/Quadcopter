5a1b1d6667894349f53f8e696bf318dddbdab780
lissettecarlr <lissettecarlr@163.com>
2016-08-03 10:47:06 +0800

    在主函数中调用了转发PC信息的函数，增加了PB5作为LED灯，测试了遥控器丢失情况--上电丢失则全为0，中途丢失数据将保持丢失时刻的数据

--- Software/Embedded/remote control conversion/template/MDK/stm32103c8t6/app/Main.cpp
+++ Software/Embedded/remote control conversion/template/MDK/stm32103c8t6/app/Main.cpp
@@ -21,7 +21,7 @@ ADC pressure(1); //PA1读取AD值
 //flash InfoStore(0x08000000+100*MEMORY_PAGE_SIZE,true);     //flash
 
 //GPIO ledRedGPIO(GPIOB,0,GPIO_Mode_Out_PP,GPIO_Speed_50MHz);//LED GPIO
-//GPIO ledBlueGPIO(GPIOB,1,GPIO_Mode_Out_PP,GPIO_Speed_50MHz);//LED GPIO
+GPIO ledBlueGPIO(GPIOB,5,GPIO_Mode_Out_PP,GPIO_Speed_50MHz);//LED GPIO
 //LED ledRed(ledRedGPIO);//LED red
 //LED ledBlue(ledBlueGPIO);//LED blue
 RemoteControl RC(&hunter,1,3,2,4);
@@ -33,12 +33,13 @@ int main()
 		u8 OldState=REMOTECONTROL_LOCK;
 		double Receive_data=0;  //接收数据  10ms
 		double RcUpdata=0;      //遥控器状态更新时间  20ms
+		ledBlueGPIO.SetLevel(0);
 	while(1)
 	{	
 		if(tskmgr.TimeSlice(Receive_data,0.01) ) //0.01
 		{
-			Hi.DataListening_SendPC();
-			
+			Hi.DataListening_SendPC();//监听飞机发送来的数据转发给PC
+			Hi.DataListening_SendCopter();//监听PC发送来的数据转发给飞机
 		}
 		
 		if(tskmgr.TimeSlice(RcUpdata,0.08) )
@@ -50,18 +51,27 @@ int main()
 			{
 					OldState = REMOTECONTROL_LOCK;
 					Hi.SendOrder(0XA0);
+					ledBlueGPIO.SetLevel(1);//上锁状态为亮
 			}
 			else if(State ==REMOTECONTROL_UNLOCK && OldState ==REMOTECONTROL_LOCK)//解锁
 			{	
 					Hi.SendOrder(0XA1);
 					OldState = REMOTECONTROL_UNLOCK;
+					ledBlueGPIO.SetLevel(0);//解锁状态为灭
+					//tskmgr.DelayS(1);
 			}		
-			else
-			{
-					Hi.SendData2Copter(RC.GetYawVal(),RC.GetThrottleVal(),RC.GetRollVal(),RC.GetPitchVal(),true);	
-			}
+			if(State == REMOTECONTROL_UNLOCK) //如果解锁了才向上位机发送
+					Hi.SendData2Copter(RC.GetYawVal(),RC.GetThrottleVal(),RC.GetRollVal(),RC.GetPitchVal(),true);			
+			
 			
 		}	
 		
 	}
 }
+
+
+/*
+遥控器丢失情况
+1.上电丢失，一开始就没检查到遥控器 （全为0）
+2.使用中丢失  这个比较麻烦。。断开后接收器依然发送断开时刻的数据
+*/
